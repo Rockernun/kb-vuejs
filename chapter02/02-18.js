9f278ab9d93328f9c22649909ac7493ba467af59
Rockernun <byunguk98@naver.com>
2026-03-16 17:33:57 +0900

    feat(02-18): 템플릿 리터럴 사용
    - 백틱으로 묶여진 문자열에서 템플릿 대입문(${})을 이용해 동적으로 문자열을 끼워넣어 구성할 수 있는 방법이다.

---
+++ chapter02/02-18.js
@@ -0,0 +1,13 @@
+const date1 = new Date();
+let name = '박병욱';
+let log1 = `${name}님에게 ${date1.toDateString()}에 연락했습니다.`;
+console.log(log1);
+
+let product = '아이폰17';
+let price = 2000000;
+let str = `${product}의 가격은 ${price}원 입니다.`;
+
+console.log(str);
+
+// 박병욱님에게 Mon Mar 16 2026에 연락했습니다.
+// 아이폰17의 가격은 2000000원 입니다.
