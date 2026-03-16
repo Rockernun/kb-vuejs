6da72daf34193e55c6bc9bc89c1d2862c1277933
Rockernun <byunguk98@naver.com>
2026-03-16 17:22:56 +0900

    feat(02-16): 객체 리터럴 축약 속성 문법 사용
    - 변수명과 객체 속성명이 같을 때 축약이 가능하다.

---
+++ chapter02/02-16.js
@@ -0,0 +1,8 @@
+let name = '박병욱';
+let age = 29;
+let email = 'byeonguk@kb.itsyourlife';
+
+let obj = { name, age, email };
+console.log(obj);
+
+// { name: '박병욱', age: 29, email: 'byeonguk@kb.itsyourlife' }
