039cf69be24f7c4841677cb1c59ddd5d6c4ad535
Byeong-Uk Park <114344042+Rockernun@users.noreply.github.com>
2026-03-19 17:19:58 +0900

    2번 문제

---
+++ homework/callback/ex02.js
@@ -0,0 +1,13 @@
+var logEven = function (i) {
+  console.log(i);
+};
+
+repeat(10, logEven);
+
+function repeat(n, f) {
+  for (var i = 0; i < n; i++) {
+    if (i % 2 == 0) {
+      f(i);
+    }
+  }
+}
